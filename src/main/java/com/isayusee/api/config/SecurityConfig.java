568c9712a809f566c4137fd483930cce9f22deb4
Neale Upstone <neale@nealeupstone.com>
2018-10-17 22:30:51 +0100

    Switch to resourceserver.jwt.issuer-uri for newer config with Boot 2.1

--- src/main/java/com/isayusee/api/config/SecurityConfig.java
+++ src/main/java/com/isayusee/api/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.isayusee.api.config;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -11,50 +10,92 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.*;
+
+import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
 
 @EnableWebSecurity
 @EnableConfigurationProperties
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig {
-    @EnableResourceServer
     @Configuration
-    public static class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    public static class ResourceServerConfig extends WebSecurityConfigurerAdapter {
         @Override
         public void configure(HttpSecurity http) throws Exception {
             // see https://spring.io/blog/2017/09/15/security-changes-in-spring-boot-2-0-m4
-            http
-                    .authorizeRequests()
-                    .antMatchers(HttpMethod.OPTIONS, "/**") // CORS pre-flight
-                    .permitAll()
-                    .requestMatchers(EndpointRequest.to("status", "info"))
-                    .permitAll()
-                    .requestMatchers(EndpointRequest.toAnyEndpoint())
-                    .hasRole("ACTUATOR")
-                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-                    .permitAll()
-                    .antMatchers("/**")
-                    .hasRole("USER");
+            http.oauth2ResourceServer()
+                    .jwt()
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        .and()
+            .and()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.OPTIONS, "/**").permitAll() // CORS pre-flight
+                    .requestMatchers(EndpointRequest.to("status", "info")).permitAll()
+                    .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
+                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                    .antMatchers("/**").hasRole("USER");
         }
-    }
 
-    @Bean
-    public AuthoritiesExtractor authoritiesExtractor() {
-        return map -> {
-            String nickname = (String) map.get("nickname");
-            if ("nealeu".equals(nickname)) {
-                return AuthorityUtils.createAuthorityList("ROLE_USER", "ROLE_ACTUATOR ");
-            }
-            else {
-                return AuthorityUtils.createAuthorityList("ROLE_USER");
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return new JwtAuthenticationConverter() {
+            @Override
+            protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+                Collection<GrantedAuthority> grantedAuthorities = super.extractAuthorities(jwt);
+                if ("nealeu".equals(jwt.getClaimAsString("nickname"))) {
+                    grantedAuthorities.addAll(createAuthorityList("ROLE_USER", "ROLE_ACTUATOR"));
+                }
+                else {
+                    grantedAuthorities.addAll(createAuthorityList("ROLE_USER"));
+                }
+                return grantedAuthorities;
             }
         };
     }
+}
+
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return authorities -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach(authority -> {
+                if (authority instanceof OidcUserAuthority) {
+                    OidcUserAuthority oidcUserAuthority = (OidcUserAuthority)authority;
+
+                    OidcIdToken idToken = oidcUserAuthority.getIdToken();
+                    OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
+
+                    // Map the claims found in idToken and/or userInfo
+                    // to one or more GrantedAuthority's and add it to mappedAuthorities
+
+                } else if (authority instanceof OAuth2UserAuthority) {
+                    OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority)authority;
+
+                    Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
+
+                    // Map the attributes found in userAttributes
+                    // to one or more GrantedAuthority's and add it to mappedAuthorities
+
+                }
+            });
+            return mappedAuthorities;
+        };
+    }
+
     @Bean
     public FilterRegistrationBean corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
